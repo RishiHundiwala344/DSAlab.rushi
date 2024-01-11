d2f88b268b73fa9828837989a94b0d28c6b12775
RishiHundiwala344 <153619923+RishiHundiwala344@users.noreply.github.com>
2024-01-11 21:58:53 +0530

    Create dma.c

---
+++ labweek2name/dma.c
@@ -0,0 +1,20 @@
+/* Name = rushi hundiwala
+USN=1BM22CS224 */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    int *arr_malloc;
+    int n;
+    printf("enter a number: \n");
+    scanf("%d",&n);
+
+    arr_malloc = (int *) malloc(n* sizeof(int));
+    free(arr_malloc);
+
+    int *arr_calloc;
+    arr_malloc = (int *) calloc(n, sizeof(int));
+    free(arr_calloc);
+}
